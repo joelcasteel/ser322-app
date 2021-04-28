313a77b9036f70d3c994d7247f31498a5f9c5122
Joel Casteel <joel@casteel.info>
2021-04-28 13:01:18 -0700

    Maybe delete?

---
+++ src/ui/MonsterCardholder.java
@@ -0,0 +1,36 @@
+package ui;
+
+import app.*;
+import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.Component;
+
+public class MonsterCardholder extends JPanel {
+    Encounter sourceEncounter;
+    JPanel containerPanel;
+    MonsterCard monsterCard;
+    MonsterEntry monsterEntry;
+
+    JPanel self;
+
+    public MonsterCardholder(Encounter encounter, JPanel panel, MonsterCard card, MonsterEntry entry) {
+        sourceEncounter = encounter;
+        containerPanel = panel;
+        monsterCard = card;
+        self = this;
+
+
+        JButton removeButton = new JButton("Remove");
+        removeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Remove code goes here
+                encounter.removeMonsterEntry(entry);
+                containerPanel.remove(self);
+                revalidate();
+			}
+		});
+        this.add(removeButton, BorderLayout.NORTH);
+
+    }
+}
